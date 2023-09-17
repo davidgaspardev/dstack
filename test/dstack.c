45494f72e585237aa7d0ac4e4f00bb0a31ecd2f1
David Gaspar <davidgaspar.dev@gmail.com>
2023-09-17 17:09:02 -0300

    test: call operation functions of DStack

--- test/dstack.c
+++ test/dstack.c
@@ -1,9 +1,25 @@
 #include <dstack.h>
+#include <stdio.h>
 
 int main() {
     DStack* dstack = create_dstack();
 
-    push(dstack, 10);
+    int info;
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &info);
+        switch (push(dstack, info)) {
+            case STACK_NULL:
+                printf("stack is null\n");
+                return 1;
+            default:;
+        }
+    }
+    
+    printf("TOP: %d\n", top(dstack));
+    
+    while(dstack->size > 0) {
+        printf("%d \n", pop(dstack));
+    }
 
     destroy(dstack);
 }
