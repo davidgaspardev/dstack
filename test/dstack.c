4c06736f882b8ced3a09f211a634dc7964a61f08
David Gaspar <davidgaspar.dev@gmail.com>
2023-09-18 01:23:40 -0300

    test: add interation with user to push data

--- test/dstack.c
+++ test/dstack.c
@@ -4,21 +4,33 @@
 int main() {
     DStack* dstack = create_dstack();
 
-    int info;
-    for (int i = 0; i < 5; i++) {
-        scanf("%d", &info);
-        switch (push(dstack, info)) {
-            case STACK_NULL:
-                printf("stack is null\n");
-                return 1;
-            default:;
+    int control, info;
+    do {
+        printf("Test menu\n");
+        printf("[ 1 ] Add info\n");
+        printf("[ 2 ] Exit\n");
+        printf("Enter menu option: ");
+        scanf("%d", &control);
+        fflush(stdin);
+
+        if (control == 1) {
+            printf("Enter new info: ");
+            scanf("%d", &info);
+            fflush(stdin);
+
+            switch(push(dstack, info)) {
+                case STACK_NULL:
+                    printf("[ ERR ] Stack is null");
+                    dstack = create_dstack();
+                default:;
+            }
         }
-    }
+    } while (control != 2);
     
-    printf("TOP: %d\n", peek(dstack));
+    printf("[ OK ] Current peek: %d\n", peek(dstack));
     
     while(dstack->size > 0) {
-        printf("%d \n", pop(dstack));
+        printf("[ OK ] Popped info: %d \n", pop(dstack));
     }
 
     destroy(dstack);
